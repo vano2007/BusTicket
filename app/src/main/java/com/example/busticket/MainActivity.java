902a3f688d23c96cd3709df1ff9f47149375a5a6
Eduard Angelskiy <ed_angel74@mail.ru>
2023-02-17 21:05:08 +0700

    Commit_17022023

--- app/src/main/java/com/example/busticket/MainActivity.java
+++ app/src/main/java/com/example/busticket/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.busticket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.busticket.Model.BusTickedOld;
+import com.example.busticket.Model.BusTicket;
+import com.example.busticket.Model.BusTicketChild;
 
 public class MainActivity extends AppCompatActivity {
+
+    BusTicket busTicket = new BusTicket(70,9);
+    BusTicket busTicketChild = new BusTicketChild(70,11,50);
+    BusTicket busTicketOld = new BusTickedOld(70,5,30);
+
+    private TextView busTicketTotalOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // присваивание переменным активити элементов представления activity_main
+        busTicketTotalOut = findViewById(R.id.busTicketTotalOut); // вывод информации стоимости всех билетов
+
+        busTicketTotalOut.setText(Float.toString(busTicket.ticketPriceAll() + busTicketChild.ticketPriceAll() + busTicketOld.ticketPriceAll()) + " монет");
+
+
     }
 }
